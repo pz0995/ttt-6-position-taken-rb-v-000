103731d25134abfec49673d2854e796ab9917d28
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-10 01:03:21 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+
+board = [" ", "O", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(board, index)
+    if board[index] == "X" || board[index] == "O"
+     return true
+
+    else index == " " || index == "" || index == "nil"
+     return false
+
+
+   end
+end
+
