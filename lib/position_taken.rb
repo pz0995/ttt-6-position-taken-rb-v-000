7ed65a15f0a77d71c53f396f58d2456aa9934239
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-10 00:58:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -12,9 +12,3 @@ def position_taken?(board, index)
 
    end
 end
-
-
-
-#one method to check for empty spaces and another for edge cases like no space between quotes
-# position taken has two args board and index that player wants for their turn
-# index at index  returs true or false only
